dd9e9dfc2b6600dca7a8873cb192ff7240eb0f88
monika-singh-ms <ysmonuys@gmail.com>
2025-07-31 15:38:39 +0530

    some changes in packge

--- config/database.js
+++ config/database.js
@@ -42,9 +42,12 @@ module.exports = ({ env }) => {
       // },
       connection: {
         connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', true) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
+        // ssl: env.bool('DATABASE_SSL', true) && {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+        // },
+        ssl: {
+        rejectUnauthorized: false,
+      },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
